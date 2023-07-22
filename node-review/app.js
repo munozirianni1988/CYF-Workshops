98a3137205222dd3b0ded1989193f17b9a9b122b
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-07-22 08:21:18 +0100

    remove logs

--- node-review/app.js
+++ node-review/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 // loading the express module on line one
-console.log("Log 1: ", typeof app);
+
 
 
 const server = express();
 // initialise a server
-console.log("Log 2: ", typeof server);
+
 
 
 
