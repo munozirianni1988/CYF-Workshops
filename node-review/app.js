51a28dc4da8129c849f617b97e64a96acecad1bb
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-07-22 14:55:20 +0100

    Level 400 done

--- node-review/app.js
+++ node-review/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 // loading the express module on line one
-
-
+const port = 9090;
 
 const server = express();
 // initialise a server
 
-
-
-
-server.get("/",function() {
-    response.status(201).send("");
+server.get("/", function (request, response) {
+  response.status(200).send("You've successfully reached the server");
 });
 // set up a "/" endpoint
 // it should respond with "You've successfully reached the server"
 // it should respond with status code 200
+
+const listener = server.listen(port, function () {
+  console.log("Your app is listening on port 9090");
+});
